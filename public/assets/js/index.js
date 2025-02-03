b8c6caf97fce6751b0a952bd89a0328a4a0b20b0
Adriano Miranda <adrjmiranda@gmail.com>
2025-02-02 22:08:49 -0300

    chore: created navbar color change animation

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log('Working!');
+const navbar = document.querySelector('#navbar');
+const navbarMenu = document.querySelector('.navbar-menu');
+const staticClassName = 'static';
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+        navbar === null || navbar === void 0 ? void 0 : navbar.classList.remove(staticClassName);
+        navbarMenu === null || navbarMenu === void 0 ? void 0 : navbarMenu.classList.remove(staticClassName);
+    }
+    else {
+        navbar === null || navbar === void 0 ? void 0 : navbar.classList.add(staticClassName);
+        navbarMenu === null || navbarMenu === void 0 ? void 0 : navbarMenu.classList.add(staticClassName);
+    }
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Working!');
+// Navbar
+const navbar = document.querySelector('#navbar') as HTMLDivElement;
+const navbarMenu = document.querySelector('.navbar-menu') as HTMLUListElement;
+
+const staticClassName = 'static';
+
+window.addEventListener('scroll', () => {
+	if (window.scrollY > 0) {
+		navbar?.classList.remove(staticClassName);
+		navbarMenu?.classList.remove(staticClassName);
+	} else {
+		navbar?.classList.add(staticClassName);
+		navbarMenu?.classList.add(staticClassName);
+	}
+});
