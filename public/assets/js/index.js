b8e3ff35775debd41060f28f9ef9c00bb70b6c88
Adriano Miranda <adrjmiranda@gmail.com>
2025-02-02 23:30:53 -0300

    chore: added minu for smaller screen devices

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -12,3 +12,25 @@ window.addEventListener('scroll', () => {
         navbarMenu === null || navbarMenu === void 0 ? void 0 : navbarMenu.classList.add(staticClassName);
     }
 });
+const showMenu = document.querySelector('#show-menu');
+const closeMenu = document.querySelector('#close-menu');
+const showShowButtonAndHideHideButton = () => {
+    closeMenu === null || closeMenu === void 0 ? void 0 : closeMenu.classList.remove('show');
+    closeMenu === null || closeMenu === void 0 ? void 0 : closeMenu.classList.add('hidden');
+    showMenu === null || showMenu === void 0 ? void 0 : showMenu.classList.remove('hidden');
+    showMenu === null || showMenu === void 0 ? void 0 : showMenu.classList.add('show');
+};
+const hideShowButtonAndShowHideButton = () => {
+    closeMenu === null || closeMenu === void 0 ? void 0 : closeMenu.classList.remove('hidden');
+    closeMenu === null || closeMenu === void 0 ? void 0 : closeMenu.classList.add('show');
+    showMenu === null || showMenu === void 0 ? void 0 : showMenu.classList.remove('show');
+    showMenu === null || showMenu === void 0 ? void 0 : showMenu.classList.add('hidden');
+};
+showMenu === null || showMenu === void 0 ? void 0 : showMenu.addEventListener('click', () => {
+    navbarMenu === null || navbarMenu === void 0 ? void 0 : navbarMenu.classList.toggle('show');
+    hideShowButtonAndShowHideButton();
+});
+closeMenu === null || closeMenu === void 0 ? void 0 : closeMenu.addEventListener('click', () => {
+    navbarMenu === null || navbarMenu === void 0 ? void 0 : navbarMenu.classList.toggle('show');
+    showShowButtonAndHideHideButton();
+});
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-// Navbar
+/**
+ * Navbar
+ * navigation bar color toggle
+ */
 const navbar = document.querySelector('#navbar') as HTMLDivElement;
 const navbarMenu = document.querySelector('.navbar-menu') as HTMLUListElement;
 
@@ -12,4 +15,38 @@ window.addEventListener('scroll', () => {
 		navbar?.classList.add(staticClassName);
 		navbarMenu?.classList.add(staticClassName);
 	}
+});
+
+/**
+ * Navbar
+ * Mobile Menu
+ */
+
+const showMenu = document.querySelector('#show-menu') as HTMLButtonElement;
+const closeMenu = document.querySelector('#close-menu') as HTMLButtonElement;
+
+const showShowButtonAndHideHideButton = (): void => {
+	closeMenu?.classList.remove('show');
+	closeMenu?.classList.add('hidden');
+
+	showMenu?.classList.remove('hidden');
+	showMenu?.classList.add('show');
+};
+
+const hideShowButtonAndShowHideButton = (): void => {
+	closeMenu?.classList.remove('hidden');
+	closeMenu?.classList.add('show');
+
+	showMenu?.classList.remove('show');
+	showMenu?.classList.add('hidden');
+};
+
+showMenu?.addEventListener('click', () => {
+	navbarMenu?.classList.toggle('show');
+	hideShowButtonAndShowHideButton();
+});
+
+closeMenu?.addEventListener('click', () => {
+	navbarMenu?.classList.toggle('show');
+	showShowButtonAndHideHideButton();
 });
